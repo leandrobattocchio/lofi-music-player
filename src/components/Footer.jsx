fcbc9d3521a98cfe5ed444ccc8b2cc221e987da8
Leandro Battocchio <leandrobattocchio@hotmail.com>
2023-04-07 18:34:12 -0300

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
     return (
         <div className='credits h-4 absolute bottom-2 left-3'>
-            All music made by ~
+            All music created by ~
             <a className="text-sm" href='https://www.youtube.com/channel/UCyD59CI7beJDU493glZpxgA' target='_blank' rel="noreferrer">
                 lofi geek
             </a>
